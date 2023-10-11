ab0ab2772dbc1ff201e05b9a6622e1ae4e4f3a71
André <113451004+andrehataishi@users.noreply.github.com>
2023-10-11 17:32:22 +0000

    feat: Adicionando algumas anotações

--- script.js
+++ script.js
@@ -15,6 +15,39 @@
 
 // método "parseInt" declara inteiros
 
+// Estrutura: switch-case
+
+
+// const opcao = 1;
+
+// switch(opcao) {
+//     case 1:
+//         console.log("Opcão é 1")
+//         break;
+//     case 2:
+//         console.log("Opcão é 2")
+//         break;
+//     default:
+//         console.log("Nenhuma das opções")   
+// }
+
+// if(opcao==1){
+//         console.log("Opcão é 1")
+// } else if(opcao==2){
+//         console.log("Opcão é 2")
+// }else{
+//     console.log("Nenhuma das opções")  
+// }
+
+// do {
+//     console.log("Criação de uma urna eletrônica em javascript")
+//     console.log("Criação de uma urna eletrônica em javascript")
+//     console.log("Criação de uma urna eletrônica em javascript")
+//     console.log("Criação de uma urna eletrônica em javascript")
+//     console.log("Criação de uma urna eletrônica em javascript")
+//     console.log("Criação de uma urna eletrônica em javascript")
+// } while (opcao==1)
+
 
 function calcular(){
 
